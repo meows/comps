37dc2fde377c7b2985ff58df618eb184bdee85e6
meows <meow@iceland>
2020-12-20 11:46:14 -0800

    first commit eva

---
+++ src/components/menu/menu.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Button({ text }) {
+   return (
+      <button>{text}</button>
+   )
+}
+
+function Menu({ active=1 }) {
+   <menu>
+      LOGO.
+      <section>
+         
+      </section>
+   </menu>
+}
+
+export default Button
